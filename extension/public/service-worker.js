2fec2e30823a5c4cd60e6b49de23bdcd619c0395
Glenn Streetman <glennstreetman@gmail.com>
2023-02-08 14:04:44 -0800

    setup offscreen keep alive

--- extension/public/service-worker.js
+++ extension/public/service-worker.js
@@ -1,34 +1,36 @@
 
-// This is the service worker script, which executes in its own context
-// when the extension is installed or refreshed (or when you access its console).
-// It would correspond to the background script in chrome extensions v2.
-
 async function postIP(url, data) {
-    console.log('posting IP update')
-    const formatData = JSON.stringify(data)
-    console.log(formatData)
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: formatData // body data type must match "Content-Type" header
-    })
-    const format = await response.json()
-    console.log(format)
+    try{
+        console.log('posting IP update')
+        const formatData = JSON.stringify(data)
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: formatData // body data type must match "Content-Type" header
+        })
+        const format = await response.json()
+        if (format?.msg) console.log(format.msg)
+        chrome.storage.sync.set({"myIP": data.ip})
+    } catch(err){
+        console.log('Service Worker Error Logging IP: ', err)
+    }
 }
 
-function updateIP(myIP,userId, hook, params){
-    if(myIP && userId){
-        console.log('Posting IP to remote', fetchURL)
+function updateIP(publicIP,userId, hook, params){
+    console.log('service worker updating IP info')
+    if(publicIP && userId){
+        console.log('Posting IP to remote', hook)
+        chrome.storage.sync.set({"myIP": publicIP})
 
-        if (myIP && hook){
+        if (publicIP && hook){
             const todayDate = new Date().toISOString().slice(0, 10)
             const data = {
-                ip: myIP,
+                ip: publicIP,
                 userid: userId,
                 date: todayDate,
             }
@@ -38,46 +40,50 @@ function updateIP(myIP,userId, hook, params){
     }
 }
 
-console.log("Starting up IP.SPY")
-chrome.storage.sync.get().then((result) => {
-    console.log("Local Sync Storage:", result)
-});
-
-chrome.runtime.onStartup.addListener(async function() { 
+const checkUpdate = async function() { 
+    console.log('Checking for IP Update')
     const storageIP = await chrome.storage.sync.get(["myIP"])
     const storageUserID = await chrome.storage.sync.get(["userId"])
-    const storageHook = chrome.storage.sync.get(["hook"])
-    const storageParams = chrome.storage.sync.get(["params"]).then((result) => {
-        const stringParams = result['params']
-        const convertParams = stringParams ? JSON.parse(stringParams) : false
-        return convertParams
-    });
+    const storageHook = await chrome.storage.sync.get(["hook"])
+    const storageParams = await chrome.storage.sync.get(["params"])
 
+    const stringParams = storageParams?.['params'] || false
+    const convertParams = stringParams ? JSON.parse(stringParams) : false
     const myIP = storageIP['myIP']
     const userId = storageUserID['userId']
     const hook = storageHook['hook']
-    const params = storageParams['params']
+    const params = convertParams?.['params'] || {}
 
-    updateIP(myIP,userId, hook, params)
+    const publicIPRaw = await fetch("https://api.ipify.org?format=json")
+    const publicIP = await publicIPRaw.json()
 
-    function loop() {
-        var d = new Date(),
-            h = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours() + 1, 0, 0, 0),
-            e = h - d;
-        if (e > 100) { // some arbitrary time period
-            updateIP(myIP,userId, hook, params)
-            window.setTimeout(loop, e);
-        }
-    }
-    loop()
+    if (publicIP.ip !== myIP)updateIP(publicIP.ip,userId, hook, params);
+};
 
+let count = 0
+// create the offscreen document if it doesn't already exist
+async function createOffscreen() {
+    if (await chrome.offscreen.hasDocument?.()) return;
+    await chrome.offscreen.createDocument({
+        url: 'offscreen.html',
+        reasons: ['BLOBS'],
+        justification: 'keep service worker running',
+    });
+}
+
+chrome.runtime.onStartup.addListener(() => {
+console.log('setup offscreen service worker')
+createOffscreen();
 });
+// a message from an offscreen document every 20 second resets the inactivity timer
 
-// Importing and using functionality from external files is also possible.
-// importScripts('service-worker-utils.js')
-
-// If you want to import a file that is deeper in the file hierarchy of your
-// extension, simply do `importScripts('path/to/file.js')`.
-// The path should be relative to the file `manifest.json`.
-
-// https://github.com/SimGus/chrome-extension-v3-starter
+chrome.runtime.onMessage.addListener(msg => {
+if (msg.keepAlive) {
+    console.log('keepAlive');
+    count += 1
+    if (count >=  180){
+        count = 0
+        checkUpdate()
+    }
+}
+});
