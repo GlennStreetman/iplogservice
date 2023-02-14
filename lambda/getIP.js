6e0387b047cd722ac1a73cd1f9483b081251f2a7
Glenn Streetman <glennstreetman@gmail.com>
2023-02-14 08:38:23 -0800

    update

--- lambda/getIP.js
+++ lambda/getIP.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context) => {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         "Access-Control-Allow-Headers" : 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Credentials": 'true',
     };
 
     try {
